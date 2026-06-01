486332355e2b84f642cb95c82d008419fcecb84c
kushwahnihal25-rgb <kushwahnihal25@gmail.com>
2026-06-01 13:17:44 +0530

    Debug API key

--- utils/ai_suggestions.py
+++ utils/ai_suggestions.py
@@ -14,7 +14,7 @@ print("API Key Found:", api_key is not None)
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def get_suggestions(resume_text, job_description):
     prompt = f"""
@@ -37,3 +37,5 @@ def get_suggestions(resume_text, job_description):
 
     response = model.generate_content(prompt)
     return response.text
+print("API Key Found:", api_key is not None)
+print("Key Length:", len(api_key) if api_key else 0)
